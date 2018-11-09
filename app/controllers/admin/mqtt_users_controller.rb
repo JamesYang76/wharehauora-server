99b92718db6a3606a55a540f004f596e68fa7121
“James <james.yang@rabidtech.co.nz>
2018-11-09 14:20:22 +1300

    Change code for Brenda PR

--- app/controllers/admin/mqtt_users_controller.rb
+++ app/controllers/admin/mqtt_users_controller.rb
@@ -9,7 +9,7 @@ class Admin::MqttUsersController < Admin::AdminController
   def create
     authorize :mqtt_user
     @home = policy_scope(Home).find(params[:home_id])
-    flash[:notice] = @home.provision_mqtt!
-    redirect_to admin_mqtt_users_path
+    message = @home.provision_mqtt!
+    redirect_to admin_mqtt_users_path, notice: message
   end
 end
